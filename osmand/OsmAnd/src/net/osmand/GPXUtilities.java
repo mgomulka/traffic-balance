8ee430cd57d97ecb95aa32afb677261ed0cfb73f
galysa <dgalysa@gmail.com>
2011-05-02 17:06:10 +0200

    Dodanie osmand-a

---
+++ osmand/OsmAnd/src/net/osmand/GPXUtilities.java
@@ -0,0 +1,213 @@
+package net.osmand;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import net.osmand.plus.R;
+
+import org.apache.commons.logging.Log;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlSerializer;
+
+import android.content.Context;
+import android.location.Location;
+import android.util.Xml;
+
+public class GPXUtilities {
+	public final static Log log = LogUtil.getLog(GPXUtilities.class);
+	
+	
+	private final static String GPX_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss'Z'";  //$NON-NLS-1$
+	
+	private final static NumberFormat latLonFormat = new DecimalFormat("0.00#####", new DecimalFormatSymbols(Locale.US));
+	
+	public static class TrkPt {
+		public double lat;
+		public double lon;
+		public double ele;
+		public double speed;
+		public long time;
+	}
+	
+	public static class WptPt {
+		public double lat;
+		public double lon;
+		public String name;
+		// by default
+		public long time = 0;
+	}
+	
+	public static class TrkSegment {
+		public List<TrkPt> points = new ArrayList<TrkPt>();
+	}
+	
+	public static class Track {
+		public List<TrkSegment> segments = new ArrayList<TrkSegment>();
+	}
+	
+	public static class GPXFile {
+		public List<Track> tracks = new ArrayList<Track>();
+		public List<WptPt> points = new ArrayList<WptPt>();
+	}
+	
+	
+	public static String writeGpxFile(File fout, GPXFile file, Context ctx) {
+		try {
+			SimpleDateFormat format = new SimpleDateFormat(GPX_TIME_FORMAT);
+			format.setTimeZone(TimeZone.getTimeZone("UTC"));
+			FileOutputStream output = new FileOutputStream(fout);
+			XmlSerializer serializer = Xml.newSerializer();
+			serializer.setOutput(output, "UTF-8"); //$NON-NLS-1$
+			serializer.setFeature("http://xmlpull.org/v1/doc/features.html#indent-output", true); //$NON-NLS-1$
+			serializer.startDocument("UTF-8", true); //$NON-NLS-1$
+			serializer.startTag(null, "gpx"); //$NON-NLS-1$
+			serializer.attribute(null, "version", "1.1"); //$NON-NLS-1$ //$NON-NLS-2$
+			serializer.attribute(null, "creator", Version.APP_NAME_VERSION); //$NON-NLS-1$
+			serializer.attribute("xmlns", "xsi", "http://www.w3.org/2001/XMLSchema-instance"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+			serializer.attribute("xsi", "schemaLocation", "http://www.topografix.com/GPX/1/1 http://www.topografix.com/GPX/1/1/gpx.xsd"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+			serializer.attribute(null, "xmlns", "http://www.topografix.com/GPX/1/1"); //$NON-NLS-1$ //$NON-NLS-2$
+
+			for (Track track : file.tracks) {
+				serializer.startTag(null, "trk"); //$NON-NLS-1$
+				for (TrkSegment segment : track.segments) {
+					serializer.startTag(null, "trkseg"); //$NON-NLS-1$
+					for (TrkPt p : segment.points) {
+						serializer.startTag(null, "trkpt"); //$NON-NLS-1$
+						serializer.attribute(null, "lat", latLonFormat.format(p.lat)); //$NON-NLS-1$ //$NON-NLS-2$
+						serializer.attribute(null, "lon", latLonFormat.format(p.lon)); //$NON-NLS-1$ //$NON-NLS-2$
+						serializer.startTag(null, "time"); //$NON-NLS-1$
+						serializer.text(format.format(new Date(p.time)));
+						serializer.endTag(null, "time"); //$NON-NLS-1$
+						serializer.startTag(null, "ele"); //$NON-NLS-1$
+						serializer.text(p.ele + ""); //$NON-NLS-1$
+						serializer.endTag(null, "ele"); //$NON-NLS-1$
+						if (p.speed > 0) {
+							serializer.startTag(null, "speed"); //$NON-NLS-1$
+							serializer.text(p.speed + ""); //$NON-NLS-1$
+							serializer.endTag(null, "speed"); //$NON-NLS-1$
+						}
+
+						serializer.endTag(null, "trkpt"); //$NON-NLS-1$
+					}
+					serializer.endTag(null, "trkseg"); //$NON-NLS-1$
+				}
+				serializer.endTag(null, "trk"); //$NON-NLS-1$
+			}
+			
+			for (WptPt l : file.points) {
+				serializer.startTag(null, "wpt"); //$NON-NLS-1$
+				serializer.attribute(null, "lat", latLonFormat.format(l.lat)); //$NON-NLS-1$ 
+				serializer.attribute(null, "lon", latLonFormat.format(l.lon)); //$NON-NLS-1$ //$NON-NLS-2$
+				serializer.startTag(null, "name"); //$NON-NLS-1$
+				serializer.text(l.name);
+				serializer.endTag(null, "name"); //$NON-NLS-1$
+				if (l.time != 0) {
+					serializer.startTag(null, "time"); //$NON-NLS-1$
+					serializer.text(format.format(new Date(l.time)));
+					serializer.endTag(null, "time"); //$NON-NLS-1$
+				}
+				serializer.endTag(null, "wpt"); //$NON-NLS-1$
+			}
+
+			serializer.endTag(null, "gpx"); //$NON-NLS-1$
+			serializer.flush();
+			serializer.endDocument();
+		} catch (RuntimeException e) {
+			log.error("Error saving gpx", e); //$NON-NLS-1$
+			return ctx.getString(R.string.error_occurred_saving_gpx);
+		} catch (IOException e) {
+			log.error("Error saving gpx", e); //$NON-NLS-1$
+			return ctx.getString(R.string.error_occurred_saving_gpx);
+		}
+		return null;
+	}
+	
+	
+	public static class GPXFileResult {
+		public ArrayList<List<Location>> locations = new ArrayList<List<Location>>();
+		public ArrayList<WptPt> wayPoints = new ArrayList<WptPt>();
+		// special case for cloudmate gpx : they discourage common schema
+		// by using waypoint as track points and rtept are not very close to real way
+		// such as wpt. However they provide additional information into gpx.
+		public boolean cloudMadeFile;
+		public String error;
+	}
+	
+	public static GPXFileResult loadGPXFile(Context ctx, File f){
+		GPXFileResult res = new GPXFileResult();
+		try {
+			boolean cloudMade = false;
+			XmlPullParser parser = Xml.newPullParser();
+			parser.setInput(new FileInputStream(f), "UTF-8"); //$NON-NLS-1$
+			
+			int tok;
+			Location current = null;
+			String currentName = ""; //$NON-NLS-1$
+			while ((tok = parser.next()) != XmlPullParser.END_DOCUMENT) {
+				if (tok == XmlPullParser.START_TAG) {
+					if (parser.getName().equals("copyright")) { //$NON-NLS-1$
+						cloudMade |= "cloudmade".equalsIgnoreCase(parser.getAttributeValue("", "author")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+
+					} else if (parser.getName().equals("trkseg")) { //$NON-NLS-1$
+						res.locations.add(new ArrayList<Location>());
+					} else if (parser.getName().equals("wpt") || parser.getName().equals("trkpt") || //$NON-NLS-1$//$NON-NLS-2$
+							(!cloudMade && parser.getName().equals("rtept"))) { //$NON-NLS-1$
+						// currently not distinguish different point represents all as a line
+						try {
+							currentName = ""; //$NON-NLS-1$
+							current = new Location("gpx_file"); //$NON-NLS-1$
+							current.setLatitude(Double.parseDouble(parser.getAttributeValue("", "lat"))); //$NON-NLS-1$ //$NON-NLS-2$
+							current.setLongitude(Double.parseDouble(parser.getAttributeValue("", "lon"))); //$NON-NLS-1$ //$NON-NLS-2$
+						} catch (NumberFormatException e) {
+							current = null;
+
+						}
+					} else if (current != null && parser.getName().equals("name")) { //$NON-NLS-1$
+						if (parser.next() == XmlPullParser.TEXT) {
+							currentName = parser.getText();
+						}
+					}
+				} else if (tok == XmlPullParser.END_TAG) {
+					if (parser.getName().equals("wpt") || //$NON-NLS-1$
+							parser.getName().equals("trkpt") || (!cloudMade && parser.getName().equals("rtept"))) { //$NON-NLS-1$ //$NON-NLS-2$ 
+						if (current != null) {
+							if (parser.getName().equals("wpt") && !cloudMade) { //$NON-NLS-1$
+								WptPt pt = new WptPt();
+								pt.lat = current.getLatitude();
+								pt.lon = current.getLongitude();
+								pt.name = currentName;
+								res.wayPoints.add(pt);
+							} else {
+								if (res.locations.isEmpty()) {
+									res.locations.add(new ArrayList<Location>());
+								}
+								res.locations.get(res.locations.size() - 1).add(current);
+							}
+						}
+					}
+				}
+			}
+		} catch (XmlPullParserException e) {
+			log.error("Error reading gpx", e); //$NON-NLS-1$
+			res.error = ctx.getString(R.string.error_reading_gpx);
+		} catch (IOException e) {
+			log.error("Error reading gpx", e); //$NON-NLS-1$
+			res.error = ctx.getString(R.string.error_reading_gpx);
+		}
+		
+		return res;
+	}
+
+}
---
+++ osmand/OsmAnd/voice/ru/_config.p
@@ -0,0 +1,116 @@
+:- op('==', xfy, 500).
+version(0).
+
+
+% before each announcement (beep)
+preamble - [].
+
+
+%% TURNS 
+turn('left', ['turn.ogg', delay_350, 'left.ogg']).
+turn('left_sh', ['turn_sharply.ogg', delay_350, 'left.ogg']).
+turn('left_sl', ['turn_slightly_left.ogg']).
+turn('right', ['turn.ogg', delay_350, 'right.ogg']).
+turn('right_sh', ['turn_sharply.ogg', delay_350,'right.ogg']).
+turn('right_sl', ['turn_slightly_right.ogg']).
+
+prepare_turn(Turn, Dist) == ['Prepare_to.ogg', 'in.ogg', delay_300, D, delay_300, M] :- 
+			distance(Dist) == D, turn(Turn, M).
+turn(Turn, Dist) == ['in.ogg', delay_250, D, delay_250, M] :- 
+			distance(Dist) == D, turn(Turn, M).
+turn(Turn) == M :- turn(Turn, M).
+
+
+prepare_make_ut(Dist) == ['Prepare_to.ogg', 'in.ogg', delay_300, D, delay_300,'Turn_back.ogg'] :- 
+		distance(Dist) == D.
+
+prepare_roundabout(Dist) == ['Prepare_to.ogg', 'in.ogg', delay_300, D, delay_300, 'roundabout.ogg'] :- 
+		distance(Dist) == D.
+
+make_ut(Dist) == ['in.ogg', delay_300, D, delay_300, 'Turn_back.ogg'] :- 
+			distance(Dist) == D.
+make_ut == ['Turn_back.ogg'].
+
+roundabout(Dist, _Angle, Exit) == ['in.ogg', delay_300, D, delay_300, 'roundabout.ogg', delay_250, 'DO.ogg', delay_250, E, 'the_exit.ogg'] :- 
+			distance(Dist) == D, nth(Exit, E).
+roundabout(_Angle, Exit) == ['DO.ogg', delay_250,  E, 'the_exit.ogg'] :- nth(Exit, E).
+
+and_arrive_destination == ['arrive_at_destination.ogg'].
+then == ['then.ogg', delay_350].
+reached_destination == ['you_reached.ogg',delay_250, 'TO_DESTINATION.ogg'].
+bear_right == ['bear_right.ogg'].
+bear_left == ['bear_left.ogg'].
+route_recalc(_Dist) == []. % 'recalc.ogg'
+route_new_calc(_Dist) == ['have_a_nice_trip_drive_carefully.ogg']. 
+
+go_ahead(Dist) == ['Drive.ogg', delay_250,  D]:- distance(Dist) == D.
+go_ahead == ['continue.ogg', 'stright.ogg'].
+
+%% 
+nth(1, '1st.ogg').
+nth(2, '2nd.ogg').
+nth(3, '3rd.ogg').
+nth(4, '4th.ogg').
+nth(5, '5th.ogg').
+nth(6, '6th.ogg').
+nth(7, '7th.ogg').
+nth(8, '8th.ogg').
+nth(9, '9th.ogg').
+
+%%% distance measure
+distance(Dist) == [F, 'meters.ogg'] :- Dist < 1000, dist(Dist, F).
+dist(D, '10.ogg') :-  D < 20, !.
+dist(D, '20.ogg') :-  D < 30, !.
+dist(D, '30.ogg') :-  D < 40, !.
+dist(D, '40.ogg') :-  D < 50, !.
+dist(D, '50.ogg') :-  D < 60, !.
+dist(D, '60.ogg') :-  D < 70, !.
+dist(D, '70.ogg') :-  D < 80, !.
+dist(D, '80.ogg') :-  D < 90, !.
+dist(D, '90.ogg') :-  D < 100, !.
+dist(D, '100.ogg') :-  D < 150, !.
+dist(D, '150.ogg') :-  D < 200, !.
+dist(D, '200.ogg') :-  D < 250, !.
+dist(D, '250.ogg') :-  D < 300, !.
+dist(D, '300.ogg') :-  D < 350, !.
+dist(D, '350.ogg') :-  D < 400, !.
+dist(D, '400.ogg') :-  D < 450, !.
+dist(D, '450.ogg') :-  D < 500, !.
+dist(D, '500.ogg') :-  D < 550, !.
+dist(D, '550.ogg') :-  D < 600, !.
+dist(D, '600.ogg') :-  D < 650, !.
+dist(D, '650.ogg') :-  D < 700, !.
+dist(D, '700.ogg') :-  D < 750, !.
+dist(D, '750.ogg') :-  D < 800, !.
+dist(D, '800.ogg') :-  D < 850, !.
+dist(D, '850.ogg') :-  D < 900, !.
+dist(D, '900.ogg') :-  D < 950, !.
+dist(D, '950.ogg') :-  !.
+
+
+distance(Dist) == ['more_than.ogg', '1.ogg', 'kilometr.ogg'] :- Dist < 1500.
+distance(Dist) == ['more_than.ogg', '1.5k.ogg', 'kilometra.ogg'] :- Dist < 2000.
+distance(Dist) == ['more_than.ogg', '2.ogg', 'kilometra.ogg'] :- Dist < 3000.
+distance(Dist) == ['more_than.ogg', '3.ogg', 'kilometra.ogg'] :- Dist < 4000.
+distance(Dist) == ['more_than.ogg', '4.ogg', 'kilometra.ogg'] :- Dist < 5000.
+distance(Dist) == ['more_than.ogg', '5.ogg', 'kilometrov.ogg'] :- Dist < 6000.
+distance(Dist) == ['more_than.ogg', '6.ogg', 'kilometrov.ogg'] :- Dist < 7000.
+distance(Dist) == ['more_than.ogg', '7.ogg', 'kilometrov.ogg'] :- Dist < 8000.
+distance(Dist) == ['more_than.ogg', '8.ogg', 'kilometrov.ogg'] :- Dist < 9000.
+distance(Dist) == ['more_than.ogg', '9.ogg', 'kilometrov.ogg'] :- Dist < 10000.
+distance(Dist) == ['more_than.ogg', X, 'kilometrov.ogg'] :- D is Dist/1000, dist(D, X).
+
+
+
+%% resolve command main method
+%% if you are familar with Prolog you can input specific to the whole mechanism,
+%% by adding exception cases.
+flatten(X, Y) :- flatten(X, [], Y), !.
+flatten([], Acc, Acc).
+flatten([X|Y], Acc, Res):- 
+		flatten(Y, Acc, R), flatten(X, R, Res).
+flatten(X, Acc, [X|Acc]).
+
+resolve(X, Y) :- resolve_impl(X,Z), flatten(Z, Y).
+resolve_impl([],[]).
+resolve_impl([X|Rest], List) :- resolve_impl(Rest, Tail), ((X == L) -> append(L, Tail, List); List = Tail).
